96fce69c3292778ebe89a5514e46fa82979c2110
= <d.hellstrom.94@gmail.com>
2017-11-22 11:46:30 +0100

    First attempt at nested accordions for displaying results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
-import {Loader} from 'semantic-ui-react'
+import {Accordion, Button, Form, Header, Icon, Loader, TextArea} from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
@@ -40,7 +40,7 @@ class App extends Component {
   getDescriptionList() {
     if (this.state.jsonResponse != null) {
       return (
-        <DescriptionView
+        <CharacterAccordion
           map={this.state.jsonResponse.data}
         />
       );
@@ -59,51 +59,120 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="inputLabel">Enter your text below:</h1>
-        <textarea id="inputText" rows="4" cols="50"></textarea>
-        <button className="confirmButton" type="button" onClick={() => this.extractApperances()}>Go!</button>
+        <Header as='h1' className="inputLabel">Enter your text below:</Header>
+        <Form>
+          <TextArea id="inputText"/>
+          <Button className="confirmButton" type="button" onClick={() => this.extractApperances()}>Go!</Button>
+        </Form>
         {this.getDescriptionList()}
       </div>
     );
   }
 }
 
-function DescriptionView(props) {
-  let descriptionMaps = [];
-  console.log(props.map)
-  for (var key in props.map) {
-    if (props.map[key].length > 0) {
-      let descriptions = [];
-      for (var i = 0; i < props.map[key].length; i++) {
-        if (props.map[key][i].coarsePartOfSpeech === "NOUN"){
-          descriptions.push(props.map[key][i+1].lemma + " " + props.map[key][i].form + " -- " + props.map[key][i].sentence);
-          i++;
-        } else {
-          descriptions.push(props.map[key][i].lemma + " -- " + props.map[key][i].sentence);
-        }
-      }
-      descriptionMaps.push({name: key, d: descriptions});
-    }
+class CharacterAccordion extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: -1
+    };
   }
-  return (
-    <ul>
-      {
-        descriptionMaps.map((character) => {
-          let i = 0;
-          return (<li key={character.name}>
-            {character.name} 
-              <ul>
-                {
-                  character.d.map((desc) => {
-                    return (<li key={"desc" + i++}>{desc}</li>)
-                  })
-                }
-              </ul>            
-          </li>);
-        })
+
+  handleClick = (e, titleProps) => {
+    const index = titleProps.index;
+    const activeIndex = this.state.activeIndex;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const {activeIndex} = this.state;
+    let map = this.props.map;
+    let descriptionMaps = [];
+    console.log(map)
+    for (var key in map) {
+      if (map[key].length > 0) {
+        let descriptions = [];
+        let entry = map[key]
+        for (var i = 0; i < entry.length; i++) {
+          if (entry[i].coarsePartOfSpeech === "NOUN"){
+            descriptions.push(entry[i+1].lemma + " " + entry[i].form + ";" + entry[i].sentence);
+            i++;
+          } else {
+            descriptions.push(entry[i].lemma + ";" + entry[i].sentence);
+          }
+        }
+        descriptionMaps.push({name: key, d: descriptions});
       }
-    </ul>
-  );
+    }
+    return (
+      <Accordion styled>
+        {
+          descriptionMaps.map((character, characterIndex) => {
+
+            return (
+              <div>
+                <Accordion.Title active={activeIndex===characterIndex} index={characterIndex} onClick={this.handleClick}>
+                  <Icon name='dropdown' />
+                  {character.name} 
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex===characterIndex}>
+                  {
+                    character.d.map((description, descriptionIndex) => {
+                      return (
+                        <DescriptionAccordion
+                          description = {description.split(';')[0]}
+                          sentence = {description.split(';')[1]}
+                        />
+                      )
+                    })
+                  }
+                </Accordion.Content>
+              </div>
+            )
+          })
+        }
+      </Accordion>
+    );
+  }
 }
+
+class DescriptionAccordion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: -1
+    };
+  }
+
+  handleClick = (e, titleProps) => {
+    const index = titleProps.index;
+    const activeIndex = this.state.activeIndex;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render () {
+    const {activeIndex} = this.state;
+    return (
+      <Accordion>
+        <Accordion.Title active={activeIndex=== 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          {this.props.description}
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex=== 0}>
+          {this.props.sentence}
+        </Accordion.Content>
+      </Accordion>
+    );
+  }
+}
+
+
 
 export default App;
